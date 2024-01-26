ca6f550105f104d6e6919812f0d1ff3c23f1bd30
VDA97 <victor.davi@hotmail.com>
2024-01-26 09:34:24 -0300

    Add Flags and Validate Argument's method

--- main.cpp
+++ main.cpp
@@ -1,28 +1,57 @@
+#include <algorithm> //It is need for using std::count.
+#include <array>
 #include <iomanip>
 #include <iostream>
 
 #include "ArgumentParser.h"
 
+// So, for avoiding typing errors, is highly recommended to organize the string arguments.
+// This project will organize this string arguments using namespaces.
+// This is also used for centralized the infos. For example, it is easy to translate this App for multiple languages
+namespace Args {
+namespace Flags {
+static constexpr const char *Rename = "rename";
+static constexpr const char *Convert = "convert";
+static constexpr const char *Resize = "resize";
+static constexpr const char *Scale = "scale";
+} // namespace Flags
+} // namespace Args
+
+void ValidateArguments(const ArgumentParser &argParser) {
+  // Ler as flags que o ArgumentParser identificou
+  const bool bRenameMood = argParser.GetFlag(Args::Flags::Rename);
+  const bool bConvertMood = argParser.GetFlag(Args::Flags::Convert);
+  const bool bResizeMood = argParser.GetFlag(Args::Flags::Resize);
+  const bool bScaleMood = argParser.GetFlag(Args::Flags::Scale);
+
+  const std::array<bool, 4> modes = {bRenameMood, bConvertMood, bResizeMood, bScaleMood};
+  const std::ptrdiff_t numActiveModes = std::count(std::begin(modes), std::end(modes), true);
+  // Verificar se somente um modo do PhotoBatch esta ativo, utilizand o OU EXCLUSIVO "^"
+  // if (!(bRenameMood ^ bConvertMood ^ bResizeMood ^ bScaleMood)) {
+  if (numActiveModes != 1) {
+    // Se houver mais de um modo ativo, lança uma excessão
+    throw std::invalid_argument("Somente um modo pode estar ativo");
+  }
+};
+
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "pt_BR");
   setlocale(LC_NUMERIC, "en_US");
   ArgumentParser argParser;
-  argParser.RegisterFlag("rename");
-  argParser.RegisterFlag("convert");
-  argParser.RegisterFlag("resize");
-  argParser.RegisterFlag("scale");
+  argParser.RegisterFlag(Args::Flags::Rename);
+  argParser.RegisterFlag(Args::Flags::Convert);
+  argParser.RegisterFlag(Args::Flags::Resize);
+  argParser.RegisterFlag(Args::Flags::Scale);
   argParser.RegisterOption("folder");
   argParser.RegisterOption("amount");
 
   argParser.Parse(argc, argv);
 
-  std::cout << std::boolalpha << "Rename  :" << argParser.GetFlag("rename") << std::endl;
-  std::cout << std::boolalpha << "Convert :" << argParser.GetFlag("convert") << std::endl;
-  std::cout << std::boolalpha << "Resize  :" << argParser.GetFlag("resize") << std::endl;
-  std::cout << std::boolalpha << "Scale   :" << argParser.GetFlag("scale") << std::endl;
-  std::cout << std::boolalpha << "Empty   :" << argParser.GetFlag("empty") << std::endl;
-  std::cout << "Amount(int):" << argParser.GetOptionAs(1, "amount") << std::endl;
-  std::cout << "Amount(float)  :" << argParser.GetOptionAs(1.1f, "amount") << std::endl;
+  try {
+    ValidateArguments(argParser);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
 
   return 0;
 }

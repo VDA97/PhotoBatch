ea09ee7c4caee890d827603959906245f679c6d7
Victor Davi Almeida <victor.davi@hotmail.com>
2024-02-13 13:29:00 -0300

    Implement ConvertMode Class

--- ConvertMode.cpp
+++ ConvertMode.cpp
@@ -51,7 +51,46 @@ void ConvertMode::RunImpl() {
 	const std::filesystem::path fromExtension = "." + ToString(m_FromFormat);
 	
 	for (const std::filesystem::path& filepath : GetFiles(fromExtension)) {
-		std::cout << GetModeName() << filepath << std::endl;
+		std::cout << GetModeName() << "Convertendo" << filepath << std::endl;
+		//Stb library will require these variables
+		int width = 0;
+		int height = 0;
+		int numComp = 0;//
+		const int numReqComp = 3;//Usually images could RGB, or RGBA (some png extensions uses RGBA). jpg does not support transparency, so Alfa could be ignored.
+		//It is pretty common to see unsigned char being used for image processing. Each pixel has an RGB. Each RGB values goes from 0 to 255, same as char.
+		std::cout << GetModeName() << "Variables" << filepath << std::endl;
+		if (unsigned char* data = stbi_load(filepath.string().c_str(), &width, &height, &numComp, numReqComp)) {
+			std::cout << GetModeName() << "IsLoadProblem ?" << filepath << std::endl;
+			std::filesystem::path destFilePath = filepath;
+			destFilePath.replace_extension(ToString(m_ToFormat));
+
+			int writeResult = 0;
+			switch (m_ToFormat)
+			{
+			case ConvertMode::Format::JPG:
+				std::cout << GetModeName() << "IsJpg ?" << filepath << std::endl;
+				writeResult = stbi_write_jpg(destFilePath.string().c_str(), width, height, numComp, data, 50);//Las parameters is regarding jpg quality. Could be a global variable to be defined by user. Todo
+			//	std::cout << "Ending for" << std::endl;
+				break;
+			case ConvertMode::Format::PNG:
+				std::cout << GetModeName() << "IsPngProblem ?" << filepath << std::endl;
+				writeResult = stbi_write_png(destFilePath.string().c_str(), width, height, numComp, data, 0);
+			//	std::cout << "Ending for" << std::endl;
+				break;
+			default:
+				break;
+			}
+
+			//writeResult = 1;
+			if (writeResult == 0) {
+				std::cout << GetModeName() << "Erro ao converter" << filepath << std::endl;
+			}
+			stbi_image_free(data);
+		}
+		else {
+			std::cout << GetModeName() << "Erro ao carregar" << filepath << std::endl;
+		}
+		std::cout << "Ending for" << std::endl;
 	}
 
 }
